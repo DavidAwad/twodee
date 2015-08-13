ea378498941744b661decdc7cba1258e369945b4
David Awad <Davidawad64@gmail.com>
2015-08-13 16:35:01 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,5 +1,8 @@
+# import the twodee module
 import twodee
 
+# create a twodee graph object
 gr = twodee.graph([0,1,2,3,4,5,6], [0,1,4,9,16,25,36])
 
+# optimize with a skip value of 1.
 print gr.optimize(1)
